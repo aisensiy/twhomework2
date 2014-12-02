f01847014ae1117beae252e437924233c3cf887f
aisensiy <aisensiy@gmail.com>
2014-12-02 23:40:23 +0800

    add test for parse intergalactic unit and roman map rule

---
+++ spec/input_processoer_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+require 'input_processor'
+
+describe InputProcessor do
+  context "parse intergalactic and roman rule" do
+    it "should parse '(word) is (roman)' sentences to {word => roman}" do
+      input = [
+        'glob is I',
+        'prok is V',
+        'pish is X',
+        'tegj is L',
+        'glob glob Silver is 34 Credits',
+        'glob prok Gold is 57800 Credits',
+        'pish pish Iron is 3910 Credits',
+        'how much is pish tegj glob glob ?',
+        'how many Credits is glob prok Silver ?',
+        'how many Credits is glob prok Gold ?',
+        'how many Credits is glob prok Iron ?',
+        'how much wood could a woodchuck chuck if a woodchuck could chuck wood ?'
+      ]
+      processor = InputProcessor.new
+      expect(processor.parse_intergalactic_map_rules(input)).to be_eql({'glob' => 'I', 'prok' => 'V', 'pish' => 'X', 'tegj' => 'L'})
+    end
+  end
+end
