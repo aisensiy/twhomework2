82c99d0c6e6d0bfbe82b5e016dc7a7da6d9d0cf6
aisensiy <aisensiy@gmail.com>
2014-12-03 00:00:29 +0800

    add test for parse query rules in input

--- spec/input_processoer_spec.rb
+++ spec/input_processoer_spec.rb
@@ -47,4 +47,45 @@ describe InputProcessor do
       ])
     end
   end
+
+  context "parse query" do
+    it "should parse 'how much is (intergalactic unit) ?' to intergalactic unit" do
+      input = [
+        'glob is I',
+        'prok is V',
+        'pish is X',
+        'tegj is L',
+        'glob glob Silver is 34 Credits',
+        'glob prok Gold is 57800 Credits',
+        'pish pish Iron is 3910 Credits',
+        'how much is pish tegj glob glob ?'
+      ]
+      processor = InputProcessor.new
+      expect(processor.parse_query_rules(input)).to be_eql([
+        'pish tegj glob glob'
+      ])
+    end
+
+    it "should parse 'how many Credits is (intergalactic unit metal) ?' to intergalactic unit metal" do
+      input = [
+        'glob is I',
+        'prok is V',
+        'pish is X',
+        'tegj is L',
+        'glob glob Silver is 34 Credits',
+        'glob prok Gold is 57800 Credits',
+        'pish pish Iron is 3910 Credits',
+        'how many Credits is glob prok Silver ?',
+        'how many Credits is glob prok Gold ?',
+        'how many Credits is glob prok Iron ?',
+        'how much wood could a woodchuck chuck if a woodchuck could chuck wood ?'
+      ]
+      processor = InputProcessor.new
+      expect(processor.parse_query_rules(input)).to be_eql([
+        'glob prok Silver',
+        'glob prok Gold',
+        'glob prok Iron'
+      ])
+    end
+  end
 end
