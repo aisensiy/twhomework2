a85fb10100a323ef1d9d69784f945bff3368fd11
aisensiy <aisensiy@gmail.com>
2014-12-02 20:26:12 +0800

    single roman number to decimal

---
+++ lib/roman_translater.rb
@@ -0,0 +1,3 @@
+class RomanTranslater
+
+end
---
+++ spec/romain_translater_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+require 'roman_translater'
+
+describe RomanTranslater do
+  context "single roman number to decimal" do
+    it "should convert I to 1" do
+      translater = RomanTranslater.new
+      expect(translater.roman_to_decimal("I")).to be_eql 1
+    end
+    it "should convert V to 5" do
+      translater = RomanTranslater.new
+      expect(translater.roman_to_decimal("V")).to be_eql 5
+    end
+    it "should convert X to 10" do
+      translater = RomanTranslater.new
+      expect(translater.roman_to_decimal("X")).to be_eql 10
+    end
+    it "should convert L to 50" do
+      translater = RomanTranslater.new
+      expect(translater.roman_to_decimal("L")).to be_eql 50
+    end
+    it "should convert C to 100" do
+      translater = RomanTranslater.new
+      expect(translater.roman_to_decimal("C")).to be_eql 100
+    end
+    it "should convert D to 500" do
+      translater = RomanTranslater.new
+      expect(translater.roman_to_decimal("D")).to be_eql 500
+    end
+    it "should convert M to 1000" do
+      translater = RomanTranslater.new
+      expect(translater.roman_to_decimal("M")).to be_eql 1000
+    end
+  end
+end
