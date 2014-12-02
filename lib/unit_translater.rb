a0216fee18a8a32a4f48af8c35a0838b14445dc9
aisensiy <aisensiy@gmail.com>
2014-12-03 00:31:17 +0800

    bugfix: convert integer to float when calculate unit credits

--- lib/unit_translater.rb
+++ lib/unit_translater.rb
@@ -13,7 +13,7 @@ class UnitTranslater
     words = unit.scan(/\w+/)
     metal = words[-1]
     roman = words[0...-1].map { |v| @map[v] }.join()
-    @metal_unit_credit[metal] = credit / @roman_translater.roman_to_decimal(roman)
+    @metal_unit_credit[metal] = 1.0 * credit / @roman_translater.roman_to_decimal(roman)
   end
 
   def cal_metal_credit(metal)
@@ -26,7 +26,7 @@ class UnitTranslater
       decimal = @roman_translater.roman_to_decimal(roman)
       return nil if decimal.nil?
       credits = metal_unit_credit[words[-1]] * decimal
-      "#{credits} Credits"
+      "#{credits.to_i} Credits"
     else
       roman = words.map do |v|
         return if !@map.include?(v)
--- spec/unit_translater_spec.rb
+++ spec/unit_translater_spec.rb
@@ -5,7 +5,7 @@ describe UnitTranslater do
   it "should convert metal unit to credit per unit metal" do
     translater = UnitTranslater.new({'glob' => 'I'})
     translater.unit_to_credit("glob glob Silver", 34)
-    expect(translater.metal_unit_credit).to be_eql({'Silver' => 17})
+    expect(translater.metal_unit_credit).to be_eql({'Silver' => 17.to_f})
   end
 
   context "calculate credits of a metal" do
