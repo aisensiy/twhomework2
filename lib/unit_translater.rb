71919d3a26eca4fc17c59d8767ab149138ab9acf
aisensiy <aisensiy@gmail.com>
2014-12-02 23:00:28 +0800

    mv unit_to_credit result as a attr of UnitTranslater

--- lib/unit_translater.rb
+++ lib/unit_translater.rb
@@ -1,15 +1,18 @@
 require 'roman_translater'
 
 class UnitTranslater
+  attr_accessor :metal_unit_credit
+
   def initialize(params)
     @map = params
     @roman_translater = RomanTranslater.new
+    @metal_unit_credit = {}
   end
 
   def unit_to_credit(unit, credit)
     words = unit.scan(/\w+/)
     metal = words[-1]
     roman = words[0...-1].map { |v| @map[v] }.join()
-    [metal, credit / @roman_translater.roman_to_decimal(roman)]
+    @metal_unit_credit[metal] = credit / @roman_translater.roman_to_decimal(roman)
   end
 end
--- spec/unit_translater_spec.rb
+++ spec/unit_translater_spec.rb
@@ -4,6 +4,7 @@ require 'unit_translater'
 describe UnitTranslater do
   it "should convert metal unit to credit per unit metal" do
     translater = UnitTranslater.new({'glob' => 'I'})
-    expect(translater.unit_to_credit("glob glob Silver", 34)).to be_eql ["Silver", 17]
+    translater.unit_to_credit("glob glob Silver", 34)
+    expect(translater.metal_unit_credit).to be_eql({'Silver' => 17})
   end
 end
