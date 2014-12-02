6b37ecb49eecd16a6b2440fd6ce4f9661880beeb
aisensiy <aisensiy@gmail.com>
2014-12-02 22:48:02 +0800

    implement calculate metal unit credits

--- lib/unit_translater.rb
+++ lib/unit_translater.rb
@@ -1,3 +1,15 @@
-class UnitTranslater
+require 'roman_translater'
 
+class UnitTranslater
+  def initialize(params)
+    @map = params
+    @roman_translater = RomanTranslater.new
+  end
+
+  def unit_to_credit(unit, credit)
+    words = unit.scan(/\w+/)
+    metal = words[-1]
+    roman = words[0...-1].map { |v| @map[v] }.join()
+    [metal, credit / @roman_translater.roman_to_decimal(roman)]
+  end
 end
