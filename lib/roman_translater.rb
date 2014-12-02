08228b65727a01bc293c84627fa29182fdbfb77c
aisensiy <aisensiy@gmail.com>
2014-12-02 20:55:03 +0800

    implement multi roman number with substract

--- lib/roman_translater.rb
+++ lib/roman_translater.rb
@@ -6,14 +6,20 @@ class RomanTranslater
     "L" => 50,
     "C" => 100,
     "D" => 500,
-    "M" => 1000
+    "M" => 1000,
+    "IV" => 4,
+    "IX" => 9,
+    "XL" => 40,
+    "XC" => 90,
+    "CD" => 400,
+    "CM" => 900
   }
 
   def roman_to_decimal(roman)
     if roman.size == 1
       @@map[roman]
     else
-      roman.scan(/\w/).map do |ch|
+      roman.scan(/IV|IX|XL|XC|CD|CM|\w/).map do |ch|
         @@map[ch]
       end.reduce(&:+)
     end
