69ad0529e98f281847ae221ec3753db2a370125e
aisensiy <aisensiy@gmail.com>
2014-12-02 23:49:17 +0800

    implement parse intergalactic and roman mapping rules

--- lib/input_processor.rb
+++ lib/input_processor.rb
@@ -1,3 +1,13 @@
 class InputProcessor
-
+  def parse_intergalactic_map_rules(input)
+    pn = /^(?<intergalactic>\w+) is (?<roman>\w+)$/
+    rules = {}
+    input.each do |line|
+      match_result = pn.match(line)
+      if !match_result.nil?
+        rules[match_result[:intergalactic]] = match_result[:roman]
+      end
+    end
+    rules
+  end
 end
