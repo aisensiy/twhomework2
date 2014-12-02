0f25287a439d17d98423af8dc53fd695daec8390
aisensiy <aisensiy@gmail.com>
2014-12-02 20:34:26 +0800

    implement single roman translate

--- lib/roman_translater.rb
+++ lib/roman_translater.rb
@@ -1,3 +1,15 @@
 class RomanTranslater
+  @@map = {
+    "I" => 1,
+    "V" => 5,
+    "X" => 10,
+    "L" => 50,
+    "C" => 100,
+    "D" => 500,
+    "M" => 1000
+  }
 
+  def roman_to_decimal(roman)
+    @@map[roman]
+  end
 end
